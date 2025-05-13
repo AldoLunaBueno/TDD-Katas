28eef5866353b24c3d3c4df86d1b6e0bf1658c94
AldoLunaBueno <albanovaphi7@gmail.com>
2025-05-13 17:25:28 -0500

    feat(fizz_buzz): implementation for multiples of 3 and 5

--- fizz_buzz/src/fizz_buzz.py
+++ fizz_buzz/src/fizz_buzz.py
@@ -1,2 +1,9 @@
 def fizz_buzz(number: int):
-    return str(number)
+    if number % 5 != 0 and number % 3 != 0:
+        return str(number)
+    elif number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
