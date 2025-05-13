f5cbfc6e99decba85c59bd3a79b33be6ff9fcb38
AldoLunaBueno <albanovaphi7@gmail.com>
2025-05-13 18:09:31 -0500

    feat(fizz_buzz): refactor of the conditional logic, from specific to general

--- fizz_buzz/src/fizz_buzz.py
+++ fizz_buzz/src/fizz_buzz.py
@@ -1,9 +1,8 @@
 def fizz_buzz(number: int):
-    if number % 5 != 0 and number % 3 != 0:
-        return str(number)
-    elif number % 3 == 0 and number % 5 == 0:
+    if number % 3 == 0 and number % 5 == 0:
         return "FizzBuzz"
     elif number % 3 == 0:
         return "Fizz"
     elif number % 5 == 0:
         return "Buzz"
+    return str(number)
