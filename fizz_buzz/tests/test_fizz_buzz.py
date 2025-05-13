b92f2ca30707e0a54513236425ea0ccb2349b872
AldoLunaBueno <albanovaphi7@gmail.com>
2025-05-13 17:21:59 -0500

    test(fizz_buzz): test return fizz or buzz or both for multiples of 3 or 5 or both respectively

--- fizz_buzz/tests/test_fizz_buzz.py
+++ fizz_buzz/tests/test_fizz_buzz.py
@@ -3,3 +3,15 @@ from src.fizz_buzz import fizz_buzz
 
 def test_returns_number_when_not_multiple_of_3_or_5():
     assert fizz_buzz(1) == "1"
+
+
+def test_returns_fizz_when_multiple_of_3():
+    assert fizz_buzz(3) == "Fizz"
+
+
+def test_returns_buzz_when_multiple_of_5():
+    assert fizz_buzz(5) == "Buzz"
+
+
+def test_returns_fizzbuzz_when_multiple_of_3_and_5():
+    assert fizz_buzz(15) == "FizzBuzz"
